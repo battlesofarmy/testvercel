5c26910583ec802736808136c670bb50798983b4
battlesofarmy <battlesofarmy@gmail.com>
2025-04-09 19:28:44 +0600

    test

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Use Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://todo:yBcFt77J5DJehPCX@cluster0.whohm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://todo:yBcFt77J5DJehPCX@cluster0.whohm.mongodb.net/<database>?retryWrites=true&w=majority"
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const todoCollection = client.db('ToDoWebApp').collection('todos');
+
+
+     // Get all todos
+     app.get('/todos', async (req, res) => {
+        try {
+            const cursor = todoCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        } catch (error) {
+            console.error('Error fetching todos:', error);
+            res.status(500).send({ success: false, message: 'Server error.' });
+        }
+    });
+
+
+    // Get all todos
+    app.get('/fahim', async (req, res) => {
+        res.send("hello fahim")
+    });
+  
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res)=>{
@@ -14,20 +71,17 @@ app.get('/', (req, res)=>{
 })
 
 
-app.get('/johfa', (req, res)=>{
-    res.send('I am johfa tahsin');
-    console.log('World');
-})
 
 
 
 app.get('/fahim', (req, res)=>{
-    res.send('I am fahim');
-    console.log('World');
+  res.send('Johfa tahsin');
+  console.log('World');
 })
 
 
 
 app.listen(port, ()=>{
-   console.log('Server is Running in Port: ', port);
+   console.log('Server is Running in Port: 3000');
 });
+
